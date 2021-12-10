28759350fd6d8f55af7e1a8bdd406723b6c59ed0
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2021-12-10 20:01:29 +0100

    finalSteepsUsuario

--- src/app/userPage/config-usuario/config-usuario.component.ts
+++ src/app/userPage/config-usuario/config-usuario.component.ts
@@ -1,15 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrentUserService } from 'src/app/current-user.service';
+import { Usuario } from '../../perfils-usuari/interfaces/interfaces';
 
 @Component({
   selector: 'app-config-usuario',
   templateUrl: './config-usuario.component.html',
   styleUrls: ['./config-usuario.component.css']
 })
-export class ConfigUsuarioComponent implements OnInit {
+export class ConfigUsuarioComponent implements OnInit
+{
+    currentUser: Usuario = { nombre: 'Mohlotov', apellidos: 'Sacarias Sacarosa', edad: 73, foto: 'https://yt3.ggpht.com/ytc/AKedOLQmuX7Nmw8pyhrSMmNDba_Hj-xQ2l0euXv9Siztnw=s900-c-k-c0x00ffffff-no-rj', correo: 'sacarina@lolopo.com', descripcion: 'Me gusta el cafe', contrasenya: 'cafe', rol: false, id: 0, amigos: [ 2, 3 ] };
 
-  constructor() { }
+nombre: string = "";
+  apellidos: string = "";
+  edad: number = 0;
+  correo: string = "";
+  foto: string = "";
+  descripcion: string = "";
+  contrasenya:string="";
 
-  ngOnInit(): void {
+  constructor(private CurrentUserService:CurrentUserService) { }
+
+  ngOnInit (): void
+  {
+    this.currentUser = this.CurrentUserService.getCurrent();
+    this.nombre = this.currentUser.nombre;
+    this.edad = this.currentUser.edad;
+    this.apellidos = this.currentUser.apellidos;
+    this.contrasenya = this.currentUser.contrasenya;
+    this.foto = this.currentUser.foto;
+    this.correo = this.currentUser.correo;
+    this.descripcion=this.currentUser.descripcion
+
+  }
+
+  updateChanges (nombre:string,apellidos:string,edad:number, foto:string,correo:string,descripcion:string,contrasenya:string)
+  {
+    
   }
 
 }
