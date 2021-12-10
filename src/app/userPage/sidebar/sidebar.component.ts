e846f5018a25ffec98d072d57514bf24d2ef3b48
GavinMcGrau <64991581+GavinMcGrau@users.noreply.github.com>
2021-12-10 06:47:33 +0100

    s

--- src/app/userPage/sidebar/sidebar.component.ts
+++ src/app/userPage/sidebar/sidebar.component.ts
@@ -1,17 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import { CurrentUserService } from '../../current-user.service';
+import { Usuario } from 'src/app/perfils-usuari/interfaces/interfaces';
 
-@Component({
+
+@Component( {
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
-})
-export class SidebarComponent implements OnInit {
+  styleUrls: [ './sidebar.component.css' ]
+} )
+export class SidebarComponent implements OnInit
+{
+  currentUser: Usuario = { nombre: 'Mohlotov', apellidos: 'Sacarias Sacarosa', edad: 73, foto: 'https://yt3.ggpht.com/ytc/AKedOLQmuX7Nmw8pyhrSMmNDba_Hj-xQ2l0euXv9Siztnw=s900-c-k-c0x00ffffff-no-rj', correo: 'sacarina@lolopo.com', descripcion: 'Me gusta el cafe', contrasenya: 'cafe', rol: false };
 
-  constructor() { 
-    
+  constructor ( private CurrentUserService: CurrentUserService )
+  {
+
   }
 
-  ngOnInit(): void {
+  ngOnInit (): void
+  {
+    this.currentUser = this.CurrentUserService.getCurrent();
+
   }
 
 }
